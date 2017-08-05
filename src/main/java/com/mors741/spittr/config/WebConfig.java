0436e9ea9fb5fd20159d9f21da58acc236ad28fe
mors741 <mors741@yandex.ru>
2017-08-05 09:57:57 +0300

    l10n

--- src/main/java/com/mors741/spittr/config/WebConfig.java
+++ src/main/java/com/mors741/spittr/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.mors741.spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -30,6 +32,13 @@ public class WebConfig {
                 registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
             }
         };
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,13 +1,13 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s" %>
 <%@ page session="false" %>
 <html>
 <head>
     <title>Spittr</title>
-    <link rel="stylesheet" type="text/css" href="<c:url value="/resources/style.css"/>"/>
+    <link rel="stylesheet" type="text/css" href="<s:url value="/resources/style.css"/>"/>
 </head>
 <body>
-<h1>Welcome to Spittr</h1>
-<a href="<c:url value="/spittles"/>">Spittles</a>
-<a href="<c:url value="/spitter/register"/>">Register</a>
+<h1><s:message code="spittr.message"/></h1>
+<a href="<s:url value="/spittles"/>">Spittles</a>
+<a href="<s:url value="/spitter/register"/>">Register</a>
 </body>
 </html>
